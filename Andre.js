0fac64a1afb6ca0bdd3220853e66bcf20dd47fe2
aleodoro93 <aleodoro93@gmail.com>
2023-06-30 21:14:55 -0300

    Ajuste de request dentro da função

--- Andre.js
+++ Andre.js
@@ -1,15 +1,15 @@
 var victor = "Maluco"
 
-function mestreSabe(){
+function mestreSabe() {
 
-if (victor.length === 6){
+    if (victor.length === 6) {
 
-    alert("maluco")
+        alert("maluco")
 
-}else {
+    } else {
 
-    alert("Feeda de shaco, feliz da vida")
-}
+        alert("Eu conheci um cara andrógino quando tava no colégio. Ele é um dos caras mais valentes que eu já conheci. Pink Bobby Jenkins. Ele era andrógino muito antes disso virar moda. Todo mundo tirava onda com ele. Aí um dia eu estava voltando pra casa da escola e uns caras me pegaram, me pegaram pra valer. Mas no meio da confusão, lá vem o Pink Bobby. Caramba! Pou pou, Pink Bobby deu uma surra naqueles caras. Eu perguntei por que ele me ajudou, já que eu não falava com ele... E ele disse que não se importava com essas coisas. Mas é porque eu nunca falei mal dele. E depois disso, ficamos amigos.")
+    }
 
 
 }
